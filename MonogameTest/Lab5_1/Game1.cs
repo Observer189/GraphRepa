b1209124919d10edce3a08c7ba9545af4054335a
Taishi-sama <alexsandr.2002.m@gmail.com>
2023-10-11 19:25:13 +0300

    Task 1.а & 1.б

--- MonogameTest/Lab5_1/Game1.cs
+++ MonogameTest/Lab5_1/Game1.cs
@@ -1,6 +1,11 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using MLEM.Input;
+using MonoGame.Extended;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Lab5_1
 {
@@ -9,7 +14,72 @@ namespace Lab5_1
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
         public SpriteFont font;
-
+        public List<(Vector2, Vector2, Color, float thickness)> Edges = new();
+        public InputHandler inputHandler;
+        public int currentParser = 0;
+        public List<Parser> parsers = new List<Parser>
+        {
+            new ("""
+                F 60 270
+                F -> F-F++F-F
+                """),
+            new ("""
+                F++F++F 60 180
+                F -> F-F++F-F
+                """),
+            new ("""
+                F+F+F+F 90 0
+                F -> F+F-F-FF+F+F-F
+                """),
+            new ("""
+                FXF--FF--FF 60 120
+                F -> FF
+                X -> --FXF++FXF++FXF--
+                """),
+            new ("""
+                YF 60 0
+                F -> F
+                X -> YF+XF+Y
+                Y -> XF-YF-X
+                """),
+            new ("""
+                X 90 0
+                F -> F
+                X -> −YF+XFX+FY−
+                Y -> +XF−YFY−FX+
+                """),
+            new ("""
+                X 90 0
+                F -> F
+                X -> X+YF+
+                Y -> -FX-Y
+                """),
+            new ("""
+                XF 60 0
+                F -> F
+                X -> X+YF++YF−FX−−FXFX−YF+
+                Y -> −FX+YFYF++YF+FX−−FX−Y
+                """),
+            new ("""
+                F 22 180
+                F -> FF-[-F+F+F]+[+F-F-F]
+                """),
+            new ("""
+                X 20 180
+                F -> FF
+                X -> F[+X]F[-X]+X
+                """),
+            new ("""
+                X 22.5 180
+                F -> FF
+                X -> F−[[X]+X]+F[+FX]−X
+                """),
+            new ("""
+                X 45 180
+                X -> F[@[-X]+X]
+                """) { StartingColor = Color.Brown, EndingColor = Color.Green, VectorForward = new Vector2(0, 30)}
+            
+        };
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
@@ -19,8 +89,9 @@ namespace Lab5_1
 
         protected override void Initialize()
         {
+            Window.AllowUserResizing = true;
             // TODO: Add your initialization logic here
-
+            inputHandler = new InputHandler(this);
             base.Initialize();
         }
 
@@ -34,20 +105,87 @@ namespace Lab5_1
 
         protected override void Update(GameTime gameTime)
         {
+            inputHandler.Update(gameTime);
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
+            //Console.WriteLine($"{GraphicsDevice.PresentationParameters.Bounds}");
 
+            if (Edges.Count == 0)
+            {
+                Edges = parsers[currentParser].GetLines();
+                Edges = ScaleToFit(Edges, GraphicsDevice.PresentationParameters.Bounds);
+            }
+            if (inputHandler.IsPressed(Keys.A)) {
+                parsers[currentParser].Iterate();
+                Edges = parsers[currentParser].GetLines();
+                Edges = ScaleToFit(Edges, GraphicsDevice.PresentationParameters.Bounds);
+            }
+            if (inputHandler.IsPressed(Keys.S))
+            {
+                parsers[currentParser].Randomness = !parsers[currentParser].Randomness;
+                Edges = parsers[currentParser].GetLines();
+                Edges = ScaleToFit(Edges, GraphicsDevice.PresentationParameters.Bounds);
+            }
+            if (inputHandler.IsPressed(Keys.R))
+            {
+                parsers[currentParser].Reset();
+                Edges = parsers[currentParser].GetLines();
+                Edges = ScaleToFit(Edges, GraphicsDevice.PresentationParameters.Bounds);
+            }
+            if (inputHandler.IsPressed(Keys.Z))
+            {
+                currentParser = currentParser - 1 < 0 ? parsers.Count - 1 : currentParser - 1;
+                Edges = parsers[currentParser].GetLines();
+                Edges = ScaleToFit(Edges, GraphicsDevice.PresentationParameters.Bounds);
+            }
+            if (inputHandler.IsPressed(Keys.X))
+            {
+                currentParser = (currentParser + 1) % parsers.Count;
+                Edges = parsers[currentParser].GetLines();
+                Edges = ScaleToFit(Edges, GraphicsDevice.PresentationParameters.Bounds);
+            }
             // TODO: Add your update logic here
+
 
             base.Update(gameTime);
         }
+        private List<(Vector2, Vector2, Color, float thickness)> ScaleToFit(List<(Vector2, Vector2, Color, float thickness)> values, Rectangle rect)
+        {
+            if (values.Count == 0) return values;
+            var dots = values.SelectMany(x => new[] { x.Item1, x.Item2 }).ToList();
+            var left = dots.Select(x => x.X).Min();
+            var rigth = dots.Select(x => x.X).Max();
+            var top = dots.Select(x => x.Y).Min();
+            var down = dots.Select(x => x.Y).Max();
+            var height = down - top;
+            var width = rigth - left;
+            var shift = new Vector2(left, top);
+            var l = values.Select(x => (x.Item1 - shift, x.Item2 - shift, x.Item3, x.thickness)).ToList();
+            if (height > rect.Height || width > rect.Width)
+            {
+                var scale = MathF.Max(height / rect.Height, width / rect.Width);
+                l = l.Select(x => (x.Item1 / scale, x.Item2 / scale, x.Item3, x.thickness * scale)).ToList();
+            }
 
+            return l;
+
+        }
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
-
-            // TODO: Add your drawing code here
-
+            _spriteBatch.Begin();
+            foreach (var edge in Edges)
+            {
+                //if (edge.Item1.)
+                //Console.WriteLine($"{edge.Item1}   {edge.Item2}");
+                _spriteBatch.DrawLine(edge.Item1, edge.Item2, edge.Item3, thickness: edge.thickness);
+            }
+            //foreach( var t in parsers[currentParser].Expression.Where(x => ((short)x) > 128))
+            //{
+            //    Console.WriteLine($"ANOMAL SYMBOL {t} (code:{((short)t)})");
+            //}
+            _spriteBatch.DrawString(font, new string(parsers[currentParser].Expression.Where(x => (ushort)x < 128).ToArray()), new Vector2(0, 300), Color.Black);
+            _spriteBatch.End();
             base.Draw(gameTime);
         }
     }
---
+++ MonogameTest/Lab5_1/Parser.cs
@@ -0,0 +1,144 @@
+﻿using Microsoft.Xna.Framework;
+using MonoGame.Extended;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5_1
+{
+    public class Parser
+    {
+        public string StartingExpression { get; private set; }
+        public float StartingAngle { get; private set; }
+        public string Expression { get; private set; }
+        public List<(string, string)> Substitutions { get; private set; }
+        public bool Randomness { get; set; }
+        public Parser(string description) {
+            var t = description.Split('\n').Select(x=>x.Trim()).ToArray();
+            var begin = t[0];
+            var beginParse = begin.Split(' ');
+            Expression = StartingExpression = beginParse[0];
+            RotationAngle = float.Parse(beginParse[1], System.Globalization.CultureInfo.InvariantCulture);
+            StartingAngle = float.Parse(beginParse[2], System.Globalization.CultureInfo.InvariantCulture);
+            Substitutions = new List<(string, string)>();
+            for (int i = 1; i < t.Length; i++)
+            {
+                var subsParse = t[i];
+                var subsSplit = subsParse.Split("->").Select(x=>x.Trim()).ToArray();
+                Substitutions.Add((subsSplit[0], subsSplit[1]));
+            }
+        }
+        
+        public Vector2 VectorForward { get; set; } = new Vector2(0, 10);
+        public int Iterations { get; private set; }
+        public float RotationAngle { get; private set; }
+        public Color StartingColor { get; set; } = Color.Black;
+        public Color EndingColor { get; set; } = Color.Black;
+        public float StartingThickness { get; set; } = 2;
+        public float ThicknessModifier { get; set; } = 0.9f;
+        public struct State
+        {
+            public float Angle { get; set; } 
+            public Vector2 Position { get; set; }
+            public float Thickness { get; set; }
+            //public Color Color { get; set; }
+            public int Iteration { get; set; } 
+            public Vector2 VectorForward { get; set; }
+        }
+        public void Reset()
+        {
+            Iterations = 0;
+            Expression = StartingExpression;
+            //Angle = StartingAngle;
+        }
+        public void Iterate()
+        {
+            foreach ((var from, var to) in Substitutions)
+            {
+                if (Expression.Contains(from))
+                {
+                    Expression = Expression.Replace(from, to);
+                }
+            }
+            Iterations++;
+        }
+        public Color GetColorProportional(float proportion)
+        {
+            proportion = Math.Clamp(proportion, 0, 1);
+            var r = (byte)Math.Clamp(StartingColor.R * (1 - proportion) + EndingColor.R * proportion, 0, 255);
+            var g = (byte)Math.Clamp(StartingColor.G * (1 - proportion) + EndingColor.G * proportion, 0, 255);
+            var b = (byte)Math.Clamp(StartingColor.B * (1 - proportion) + EndingColor.B * proportion, 0, 255);
+            return new Color(r, g, b);
+        }
+        public List<(Vector2, Vector2, Color, float thickness)> GetLines()
+        {
+            List<(Vector2, Vector2, Color, float thickness)> edges = new();
+            Stack<State> saveStates = new Stack<State>(){};
+            var curState = new State() { Angle = StartingAngle, Position = new Vector2(),
+                Thickness = this.StartingThickness, Iteration = 0, VectorForward = this.VectorForward
+            };
+            //var oldState = curState;
+            var pos = 0;
+            //var intimidation = 0;
+            var r = new Random();
+            while (true) {
+                if (pos >= Expression.Length) { break; }
+                var c = Expression[pos];
+                
+                switch (c)
+                {
+                    case 'F':
+                        {
+                            var moveForward = curState; //new State() { Angle = curState.Angle, Position = curState.Position + VectorForward.Rotate(curState.Angle * MathF.PI / 180)};
+                            moveForward.Position += moveForward.VectorForward.Rotate(curState.Angle * MathF.PI / 180);
+
+                            edges.Add((curState.Position, moveForward.Position, GetColorProportional(Iterations == 0 ? 0 : (float)moveForward.Iteration / (float)Iterations), moveForward.Thickness));
+                            curState = moveForward;
+                        }
+                        break;
+                    case '+':
+                        {
+                            if (Randomness)
+                                curState.Angle += r.NextSingle() * RotationAngle;
+                            else
+                                curState.Angle += RotationAngle;
+                        }
+                        break;
+                    case '-':
+                        {
+                            if (Randomness)
+                                curState.Angle -= r.NextSingle() * RotationAngle;
+                            else
+                                curState.Angle -= RotationAngle;
+                        }
+                        break;
+                    case '[':
+                    case '(':
+                        {
+                            saveStates.Push(curState);
+                        }
+                        break;
+                    case ']':
+                    case ')':
+                        {
+                            curState = saveStates.Pop();
+                        }
+                        break;
+                    case '@':
+                        {
+                            curState.Iteration += 1;
+                            curState.Thickness *= ThicknessModifier;
+                            curState.VectorForward *= ThicknessModifier;
+                        }
+                        break;
+                    default:
+                        break;
+                }
+                pos++;
+            }
+            return edges;
+        }
+    }
+}
