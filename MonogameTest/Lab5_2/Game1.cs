046b7761e58ef072577b3c42d99851b1e3189ab6
Taishi-sama <alexsandr.2002.m@gmail.com>
2023-10-20 17:00:52 +0300

    Lab5_2 end

--- MonogameTest/Lab5_2/Game1.cs
+++ MonogameTest/Lab5_2/Game1.cs
@@ -5,6 +5,9 @@ using System.Collections.Generic;
 using System;
 using System.Diagnostics;
 using System.Threading;
+using MLEM.Input;
+using MonoGame.Extended;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace Lab5_2
 {
@@ -26,7 +29,7 @@ namespace Lab5_2
 
         private int TerrainWidth = 1080; 
         private int TerrainHeight = 1080;
-
+        private int InitialScale = 90;
         private float[,] heightMap;
 
 
@@ -42,7 +45,17 @@ namespace Lab5_2
 
         protected override void Initialize()
         {
-            heightMap = new float[TerrainWidth, TerrainHeight];
+            inputHandler = new InputHandler(this);
+            var r = new Random();
+            heightMap = new float[TerrainWidth/InitialScale, TerrainHeight/InitialScale];
+            for (int x = 0; x < heightMap.GetLength(0); x++)
+            {
+                for (int y = 0; y < heightMap.GetLength(1); y++)
+                {
+                    heightMap[x, y] = r.NextSingle();
+                }
+
+            }
             base.Initialize();
         }
 
@@ -55,14 +68,8 @@ namespace Lab5_2
             colorData[0] = Color.White;
             whiteTexture.SetData(colorData);
         }
-
-        protected override void Update(GameTime gameTime)
-        {
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
-                Exit();
-
-            base.Update(gameTime);
-        }
+        public InputHandler inputHandler;
+        
 
         public float Val(int x, int y, float? v = null)
         {
@@ -150,37 +157,161 @@ namespace Lab5_2
 
             }
         }
-
-        protected override void Draw(GameTime gameTime)
+        private float[,] ExtendStep(float [,] heightMap)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
-
-            _spriteBatch.Begin();
-
-            for (int x = 0; x < TerrainWidth; x++)
+            var newWidth = 2 * heightMap.GetLength(0) - 1;
+            var newHeight = 2 * heightMap.GetLength(1) - 1;
+            var newHeightMap = new float[newWidth, newHeight];
+            for (int x = 0; x < heightMap.GetLength(0); x++)
             {
-                for (int y = 0; y < TerrainHeight; y++)
+                for (int y = 0; y < heightMap.GetLength(1); y++)
                 {
-                    Val(x, y);
-                    float terrainValue = heightMap[x, y]; // Используем данные из буфера
-
-                    //Debug.WriteLine($"Val at ({x}, {y}): {Val(x, y)}");
-                    //Debug.WriteLine($"terrainValue at ({x}, {y}): {terrainValue}");
-                    Color terrainColor = DetermineTerrainColor(terrainValue);
-                    Rectangle rect = new Rectangle(x, y, 1, 1);
-                    _spriteBatch.Draw(whiteTexture, rect, terrainColor);
+                    var tX = 2*x;
+                    var tY = 2*y;
+                    newHeightMap[tX, tY] = heightMap[x, y];
                 }
 
             }
+            return newHeightMap;
+        }
+        private void SquareStep(float[,] heightmap)
+        {
+            for (int x = 1;x < heightmap.GetLength(0); x += 2)
+            {
+                for (int y = 1; y < heightmap.GetLength(1); y += 2)
+                {
+                    heightMap[x,y] = (heightmap[x + 1, y + 1] + heightmap[x + 1, y - 1] + heightmap[x - 1, y + 1] + heightmap[x - 1, y - 1]) / 4;
+                }
+
+            }
+        }
+        private void DiamondStep(float[,] heightmap)
+        {
+            for (int x = 0; x < heightmap.GetLength(0);x += 2)
+            {
+                for (int y = 1; y < heightmap.GetLength(1); y += 2)
+                {
+                    if (x == 0)
+                    {
+                        heightMap[x, y] = (heightmap[x + 1, y] + heightmap[x, y + 1] + heightmap[x, y - 1]) / 3;
+                    }
+                    else if (x >= heightMap.GetLength(0) - 2){
+                        heightMap[x, y] = (heightmap[x - 1, y] + heightmap[x, y + 1] + heightmap[x, y - 1]) / 4;
+                    }
+                    else
+                    {
+                        heightMap[x, y] = (heightmap[x + 1, y] + heightmap[x - 1, y] + heightmap[x, y + 1] + heightmap[x, y - 1]) / 4;
+                    }
+                }
+            }
+            for (int x = 1; x < heightmap.GetLength(0); x += 2)
+            {
+                for (int y = 0; y < heightmap.GetLength(1); y += 2)
+                {
+                    if (y == 0)
+                    {
+                        heightMap[x, y] = (heightmap[x + 1, y] + heightmap[x, y + 1] + heightmap[x - 1, y]) / 3;
+                    }
+                    else if (y >= heightMap.GetLength(1) - 2)
+                    {
+                        heightMap[x, y] = (heightmap[x - 1, y] + heightmap[x + 1, y] + heightmap[x, y - 1]) / 4;
+                    }
+                    else
+                    {
+                        heightMap[x, y] = (heightmap[x + 1, y] + heightmap[x - 1, y] + heightmap[x, y + 1] + heightmap[x, y - 1]) / 4;
+                    }
+                }
+            }
+        }
+        int state = 0;
+        bool isAltHeightMap = false;
+        protected override void Update(GameTime gameTime)
+        {
+            inputHandler.Update(gameTime);
+
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+            if (inputHandler.IsPressed(Keys.S))
+            {
+                if (state == 0)
+                {
+                    heightMap = ExtendStep(heightMap);
+                }
+                if (state == 1)
+                {
+                    SquareStep(heightMap);
+                }
+                if (state == 2)
+                {
+                    DiamondStep(heightMap);
+                }
+                state = (state + 1) % 3;
+                heightTexture = null;
+            }
+            if (inputHandler.IsPressed(Keys.A))
+            {
+                isAltHeightMap = !isAltHeightMap;
+                heightTexture = null;
+            }
+
+                base.Update(gameTime);
+        }
+        Texture2D HeighmapToTexture(float[,] heightmap)
+        {
+            var t = new Texture2D(GraphicsDevice, TerrainWidth, TerrainHeight);
+            var c = new Color[TerrainWidth * TerrainHeight];
+            double xArr = heightMap.GetLength(0);
+            double yArr = heightMap.GetLength(1);
+            for (int y = 0; y < TerrainHeight; y++)
+                for (int x = 0; x < TerrainWidth; x++)
+                {
+                    var hX = (int)Math.Floor(Math.Clamp(xArr / TerrainWidth * x, 0, xArr - 1));
+                    var hY = (int)Math.Floor(Math.Clamp(yArr / TerrainHeight * y, 0, yArr - 1));
+                    c[TerrainWidth * y + x] = DetermineTerrainColor(heightMap[hX, hY]);
+                }
+            t.SetData(c);
+            return t;
+        }
+        Texture2D HeighmapToTextureAlt(float[,] heightmap)
+        {
+            var t = new Texture2D(GraphicsDevice, TerrainWidth, TerrainHeight);
+            var c = new Color[TerrainWidth * TerrainHeight];
+            double xArr = heightMap.GetLength(0);
+            double yArr = heightMap.GetLength(1);
+            for (int y = 0; y < TerrainHeight; y++)
+                for (int x = 0; x < TerrainWidth; x++)
+                {
+                    var hX = (int)Math.Floor(Math.Clamp(xArr / TerrainWidth * x, 0, xArr - 1));
+                    var hY = (int)Math.Floor(Math.Clamp(yArr / TerrainHeight * y, 0, yArr - 1));
+                    c[TerrainWidth * y + x] = DetermineTerrainColorAlt(heightMap[hX, hY]);
+                }
+            t.SetData(c);
+            return t;
+        }
+        Texture2D heightTexture = null;
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+            if (heightTexture == null) { heightTexture = isAltHeightMap ?  HeighmapToTextureAlt(heightMap) : HeighmapToTexture(heightMap); }
+            _spriteBatch.Begin();
+            double xArr = heightMap.GetLength(0);
+            double yArr = heightMap.GetLength(1);
+            
+            _spriteBatch.Draw(heightTexture, new Rectangle(0, 0, TerrainWidth, TerrainHeight), Color.White);
 
             _spriteBatch.End();
 
             base.Draw(gameTime);
         }
-
+        private Color DetermineTerrainColorAlt(float terrainValue)
+        {
+            Color fromColor = Color.Black;
+            Color toColor = Color.White;
+            return Color.Lerp(fromColor, toColor, terrainValue);
+        }
         private Color DetermineTerrainColor(float terrainValue)
         {
-            Color fromColor = Color.Gray;
+            Color fromColor = Color.DarkGray;
             Color toColor = Color.White;
 
             if (terrainValue < 0.2f)
