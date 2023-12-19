ee531c380acc4f3a1fcc8c8a76ce3db10c24af8a
Taishi-sama <alexandra.2002.mikh@gmail.com>
2023-12-19 19:27:52 +0300

    Shaders, phong lighting

---
+++ MonogameTest/Lab14/obj_defs.cpp
@@ -0,0 +1,75 @@
+#include "obj_defs.h";
+
+void checkOpenGLerror() {
+	GLenum err;
+	int count = 0;
+	while ((err = glGetError()) != GL_NO_ERROR)
+	{
+		auto err_text = glewGetErrorString(err);
+		std::cout << "Error: " << err << ": " << err_text << "\n";
+		// Process/log the error.
+		if (count++ > 100) break;
+	}
+}
+void shaderLog(GLuint shader)
+{
+	int infologLen = 0;
+	glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &infologLen);
+	if (infologLen > 1)
+	{
+		int charsWritten = 0;
+		std::vector<char> infoLog(infologLen);
+		glGetShaderInfoLog(shader, infologLen, &charsWritten, infoLog.data());
+		std::cout << "InfoLog: " << infoLog.data() << std::endl;
+	}
+}
+std::string stringFromFile(const char* path) {
+	std::ifstream ifs(path);
+	const std::string vsh((std::istreambuf_iterator<char>(ifs)), (std::istreambuf_iterator<char>()));
+	return vsh;
+}
+
+void GameObject::lightUpdate(Scene& scene)
+{
+	checkOpenGLerror();
+
+	auto ps = this->program.get();
+	ps->Bind();
+	auto plsPos_id = ps->GetUniformIdFallable("plsPos");
+	if (plsPos_id != -1) {
+		glUniform4fv(plsPos_id, 1, glm::value_ptr(scene.pls.lightsourcePos));
+	}
+	auto plsIntencity_id = ps->GetUniformIdFallable("plsIntencity");
+	if (plsIntencity_id != -1) {
+		glUniform1f(plsIntencity_id, scene.pls.intencity);
+	}
+
+	auto pjlsPos_id = ps->GetUniformIdFallable("pjlsPos");
+	if (pjlsPos_id != -1) {
+		glUniform4fv(pjlsPos_id, 1, &scene.pjls.projectorPos[0]);
+	}
+	auto pjlsIntencity_id = ps->GetUniformIdFallable("pjlsIntencity");
+	if (pjlsIntencity_id != -1) {
+		glUniform1f(pjlsIntencity_id, scene.pjls.intencity);
+	}
+	auto pjlsAngle_id = ps->GetUniformIdFallable("pjlsAngle");
+	if (pjlsAngle_id != -1) {
+		glUniform1f(pjlsAngle_id, scene.pjls.angle);
+	}
+	auto pjlsDir_id = ps->GetUniformIdFallable("pjlsDir");
+	if (pjlsDir_id != -1) {
+		glUniform3fv(pjlsDir_id, 1, &scene.pjls.dir[0]);
+	}
+
+	auto glsIntencity_id = ps->GetUniformIdFallable("glsIntencity");
+	if (glsIntencity_id != -1) {
+		glUniform1f(glsIntencity_id, scene.gls.intencity);
+	}
+	auto glsDir_id = ps->GetUniformIdFallable("glsDir");
+	if (glsDir_id != -1) {
+		glUniform3fv(glsDir_id, 1, &scene.gls.dir[0]);
+	}
+
+	ps->Unbind();
+	checkOpenGLerror();
+}
---
+++ MonogameTest/Lab14/subtask1.cpp
@@ -0,0 +1,102 @@
+#include <string>
+#include "subtask1.h"
+
+
+float mouseX;
+float mouseY;
+
+
+void Draw(Scene& scene) {
+	for (auto& obj : scene.objects) {
+		obj.update(scene.camera);
+		obj.lightUpdate(scene);
+		obj.vao.get()->Bind();
+		obj.texture.get()->Bind();
+		obj.program.get()->Bind();
+		glDrawElements(GL_TRIANGLES, obj.mesh.get()->indices.size(), GL_UNSIGNED_INT, 0);
+		obj.texture.get()->Unbind();
+		obj.program.get()->Unbind();
+		obj.vao.get()->Unbind();
+	}
+	checkOpenGLerror();
+}
+
+void subtask1(sf::Window& window) {
+	using namespace std::string_literals;
+	auto camera = Camera();
+	window.setMouseCursorVisible(false);
+	//window.setMouseCursorGrabbed(true);
+
+
+	glEnable(GL_DEPTH_TEST);
+	/*auto s = std::string("./objects/12140_Skull_v3_L2.obj"s);
+	std::string frag = stringFromFile("./shaders/subtask1.frag");
+	std::string vert = stringFromFile("./shaders/subtask1.vert");
+	auto prog = ProgramShader::make_from(vert, frag);
+	auto tex = std::make_shared<Texture2D>();
+	tex.get()->LoadFromPNG("./objects/Skull.png");
+	auto skullMeshPointer = std::make_shared<Object3D>(s);
+	auto skull = GameObject(skullMeshPointer, tex, prog);*/
+	auto skull = GameObject::make_from_strings("./objects/12140_Skull_v3_L2.obj"s, "./objects/Skull.png"s, "./shaders/phong.vert"s, "./shaders/phong.frag"s);
+	skull.transform.scale = glm::vec3(0.2f);
+	skull.transform.position = glm::vec3(0, -1, 0);
+	skull.transform.Rotate(glm::vec3(-90.0f, 0.f, 0.f));
+	GlobalLightSource gls(0.5f, glm::vec3(0.0, 1.0, 0.0));
+	PointLightSource pls(glm::vec4(5.0, 0.0, 0.0, 1.0), 3.0f);
+	ProjectorLightSource pjls(
+		glm::vec4(-7.0, 1.0, 0.0, 1.0), 10.0f, glm::normalize(glm::vec3(-1, 0, 0)), 50.0f);
+	auto scene = Scene(std::vector({ skull }), camera, gls, pls, pjls);
+	for (auto& obj : scene.objects) {
+		obj.init();
+	}
+		
+
+	checkOpenGLerror();
+	
+	while (window.isOpen()) {
+		sf::Event event;
+		while (window.pollEvent(event)) {
+			if (event.type == sf::Event::Closed) { window.close(); }
+			else if (event.type == sf::Event::Resized) { glViewport(0, 0, event.size.width, event.size.height); }
+
+			if (event.type == sf::Event::EventType::KeyPressed) {
+				if (event.key.code == sf::Keyboard::D) {
+					//transform = glm::translate(transform,glm::vec3(0.05f,0.0f,0.0f));
+					scene.camera.ProcessKeyboard(RIGHT,0.05f);
+
+				}
+				else if (event.key.code == sf::Keyboard::A) {
+					scene.camera.ProcessKeyboard(LEFT, 0.05f);
+
+				}
+				else if (event.key.code == sf::Keyboard::W) {
+					scene.camera.ProcessKeyboard(FORWARD, 0.05f);
+
+				}
+				else if (event.key.code == sf::Keyboard::S) {
+					scene.camera.ProcessKeyboard(BACKWARD, 0.05f);
+
+				}
+				
+
+			}
+			else if (event.type == sf::Event::MouseMoved)
+			{
+				
+				scene.camera.ProcessMouseMovement(-(mouseX - event.mouseMove.x), mouseY - event.mouseMove.y);
+				
+				mouseX = event.mouseMove.x;
+				mouseY = event.mouseMove.y;
+				
+				//std::cout << "new mouse x: " << event.mouseMove.x<< std::endl;
+				//std::cout << "new mouse y: " << event.mouseMove.y << std::endl;
+			}
+		}
+		sf::Clock clock;
+		if (window.isOpen()) {
+			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+			Draw(scene);
+			window.display();
+		}
+	}
+}
